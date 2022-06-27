80dfaaade3b64528b80365a2ceb069ad8c84e695
ZTStew <ZTStew>
2022-06-27 14:49:27 -0400

    Sensor display tests no 1

--- 1) Learning/sensor_display.py
+++ 1) Learning/sensor_display.py
@@ -3,6 +3,10 @@ Author: Zachary Stewart
 Date: 6/27/2022
 
 Description: Test project to learn the basics of Raspberry PI + Sense Hat
+  Test sensor display commands
+
+
+
 """
 
 # import SenseHat module
@@ -10,7 +14,13 @@ from sense_hat import SenseHat
 # initialize instance of SenseHat
 sense = SenseHat()
 
-sense.show_message("test")
-# sense.show_letter('z')
+# displays full message, then displays last letter, 'h'
 
-# print("test")
+# displays "test"
+sense.show_message("test")
+
+# shows only 'h'
+sense.show_letter('z')
+sense.show_letter('a')
+sense.show_letter('c')
+sense.show_letter('h')
---
+++ README.md
@@ -0,0 +1,5 @@
+Author: Zachary Stewart
+
+Description: Test project to learn the basics of Raspberry PI + Sense Hat
+
+1) Learning - tests with sensor display commands
